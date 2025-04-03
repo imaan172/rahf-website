4d5533b15629916566fb402b7b76844ce92b6464
Moses Ebenardor-Amiedor <atmdboss@yahoo.com>
2025-04-03 23:04:48 +0200

    added contact page

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Changed instances of `Link` to `NavLink`, because after checking react router docs, that's what they use in their examples, so i just think it might be better
 2. Initialised InfoPage 1.0
    - might add other things later, but for now the content exists
+3. Added Contact page with two forms
---
+++ src/components/Forms/QueryForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+const QueryForm = () => {
+  const [values, setValues] = useState({
+    fullName: '',
+    email: '',
+    phoneNumber: '',
+    message: '',
+  });
+
+  const handleChange = (event) => {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stack component="form" spacing={4} onSubmit={handleSubmit}>
+        <TextField required fullWidth variant="standard" id="fullName" label="Full Name" value={values.fullName} onChange={handleChange} />
+        <TextField required fullWidth variant="standard" id="email" label="Email Address" value={values.email} onChange={handleChange} />
+        <TextField required fullWidth variant="standard" id="phoneNumber" label="Phone Number" value={values.phoneNumber} onChange={handleChange} />
+        <TextField
+          required
+          fullWidth
+          multiline
+          rows={5}
+          variant="outlined"
+          id="message"
+          label="Message"
+          value={values.message}
+          onChange={handleChange}
+        />
+
+        <Stack direction="row" justifyContent="flex-end">
+          <Button type="submit" color="inherit" variant="outlined" sx={{ borderRadius: '20px', px: 4, textTransform: 'uppercase' }}>
+            Submit
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default QueryForm;
